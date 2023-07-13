ba61b92804523003f8f6d73cdfc8e9e895719506
vishnupriya202089 <135610599+vishnupriya202089@users.noreply.github.com>
2023-07-13 17:56:08 +0530

    Update newwebsite.py
    including DB

--- newwebsite.py
+++ newwebsite.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import pyodbc
+import pymssql
+import pandas as pd
 
 
 st.set_page_config(page_title="IoT CoE Track 'N' Trace",page_icon=":tada:",layout="wide")
@@ -14,9 +17,25 @@ with st.container():
          options=("Material Handling Station","Pinning and Press Station","Drilling Station"),
   )
 
+
+cnxn_str = ("Driver={ODBC Driver 18 for SQL Server};"
+            "Server=coepocsqlserver;"
+            "Database=coeqrappdb;"
+            "UID=coepocadmin;"
+            "PWD=Password@123;")
+cnxn = pyodbc.connect(cnxn_str)
+
+cursor = cnxn.cursor()
+
+cursor.execute("SELECT  * FROM Block_Details")
+
+data = pd.read_sql("SELECT  * FROM Block_Details", cnxn)
+
+
 if selected == "Material Handling Station":
      st.title(f"you have selected {selected}")
 
+st.table(data)
 
 if selected == "Pinning and Press Station":
   st.title(f"you have selected {selected}")
